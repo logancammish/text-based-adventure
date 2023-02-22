dd17ca06e3cd55ac182d3282d69dccceb1d28bff
logancammish <124637378+logancammish@users.noreply.github.com>
2023-02-23 09:00:25 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time 
+import json
 sleep = time.sleep
 
 options = {
@@ -7,19 +8,24 @@ options = {
     "C": "Survive your way through a frozen wasteland"
 }
 
-print("Welcome! You will be able to choose between 3 options.\n")
-sleep(1)
-for i in options: 
-    print(i + ": " + options[i])
-print("\n")
+with open("choices\savedata.json", "r") as json_file:
+    data = json.load(json_file)
+if data['choice']:
+    exec(open("choices/" + data['choice'] + ".py").read())
+else:
+    print("Welcome! You will be able to choose between 3 options.\nHowever, you may not change your answer until you complete said level.\n")
+    sleep(1)
+    for i in options: 
+        print(i + ": " + options[i])
+    print("\n")
 
-def choose():
-    answer = input("Please enter your answer now (A/B/C): ").capitalize()
-    try: 
-        print("Get ready to " + options[answer].lower() + "!\n")
-        return answer 
-    except: 
-        print("Invalid value, try again...")
-        return choose()
-choice = choose()
-exec(open("choices/" + choice + ".py").read())
+    def choose():
+        answer = input("Please enter your answer now (A/B/C): ").capitalize()
+        try: 
+            print("Get ready to " + options[answer].lower() + "!\n")
+            return answer 
+        except: 
+            print("Invalid value, try again...")
+            return choose()
+    choice = choose()
+    exec(open("choices/" + choice + ".py").read())
